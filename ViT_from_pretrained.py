b5d2a30c36449de5de28c0d9c7d3a1337629ed72
Zoë Prins <zoe.prins@hotmail.com>
2022-09-20 12:40:20 +0200

    work on pretrained ViT

--- ViT_from_pretrained.py
+++ ViT_from_pretrained.py
@@ -6,15 +6,45 @@ from transformers import ViTFeatureExtractor, ViTModel
 from PIL import Image
 import requests
 import h5py
+import numpy as np
+import evaluate
+from transformers import TrainingArguments, Trainer
 
 model = torch.hub.load("facebookresearch/dino:main", "dino_vits8")
 
+def getDataSets():
+    f = h5py.File('pcamv1/camelyonpatch_level_2_split_train_x.h5', 'r')
+    trainset_x = f['x']
+    f = h5py.File('pcamv1/camelyonpatch_level_2_split_train_y.h5', 'r')
+    trainset_y = f['y']
+    f = h5py.File('pcamv1/camelyonpatch_level_2_split_test_x.h5', 'r')
+    testset_x = f['x']
+    f = h5py.File('pcamv1/camelyonpatch_level_2_split_test_y.h5', 'r')
+    testset_y = f['y']
+    f = h5py.File('pcamv1/camelyonpatch_level_2_split_valid_x.h5', 'r')
+    validset_x = f['x']
+    f = h5py.File('pcamv1/camelyonpatch_level_2_split_valid_y.h5', 'r')
+    validset_y = f['y']
+    train_set = [{"x": torch.from_numpy(trainset_x[i]).permute(2, 0, 1).float(), "labels": trainset_y[i]} for i in range(len(trainset_y))]
+    test_set = [{"x": torch.from_numpy(testset_x[i]).permute(2, 0, 1).float(), "labels": testset_y[i]} for i in range(len(testset_y))]
+    valid_set = [{"x": torch.from_numpy(validset_x[i]).permute(2, 0, 1).float(), "labels": validset_y[i]} for i in range(len(validset_y))]
+    return train_set, test_set, valid_set
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
 
 if __name__ == '__main__':
-    images = h5py.File('pcamv1/camelyonpatch_level_2_split_train_x.h5', 'r')
-    # out = model(train_dataloader)
-    feature_extractor = ViTFeatureExtractor.from_pretrained('facebook/dino-vits8')
-    model = ViTModel.from_pretrained('facebook/dino-vits8')
-    inputs = feature_extractor(images=images, return_tensors="pt")
-    outputs = model(**inputs)
-    last_hidden_states = outputs.last_hidden_state
+    train_set, test_set, valid_set = getDataSets()
+    training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
+    metric = evaluate.load("accuracy")
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_set,
+        eval_dataset=valid_set,
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
