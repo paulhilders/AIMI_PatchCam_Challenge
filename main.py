a88593e9d642c41ef431a3fdee68c983d7d40297
Angela <angelavansprang@gmail.com>
2022-09-27 12:14:37 +0200

    ability to freeze layers + save with name

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ import settings
 from densenet_pytorch import DenseNet
 
 
-def load_model(modelname):
+def load_model(modelname, freeze=False):
     NUM_CLASSES = 2
     model = DenseNet.from_pretrained(modelname)
     model.classifier = nn.Linear(model.classifier.in_features, NUM_CLASSES)
+    if freeze:
+        for i, child in enumerate(model.children()):
+            if i == 0:
+                for param in child.parameters():
+                    param.requires_grad = False
     return model
 
 
@@ -54,7 +59,7 @@ def eval_model(model, dataloader):
     return avg_acc
 
 
-def train(model, criterion, optimizer, num_epochs, train_dataloader, val_dataloader):
+def train(model, criterion, optimizer, num_epochs, train_dataloader, val_dataloader, modelname):
     val_accuracies = []
     best_val_epoch = -1
     for epoch in range(num_epochs):
@@ -90,9 +95,10 @@ def train(model, criterion, optimizer, num_epochs, train_dataloader, val_dataloa
             best_val_epoch = epoch
 
             # Saving State Dict
-            torch.save(model.state_dict(), './best_densenet_model.pth')
+            torch.save(model.state_dict(), f'./models/{modelname}.pth')
 
-    best_model = torch.load('./best_densenet_model.pth')
+    model.load_state_dict(torch.load(f'./models/{modelname}.pth'))
+    best_model = model
     return best_model, val_accuracies
 
 
@@ -103,7 +109,7 @@ if __name__ == '__main__':
     print("Dataloaders loaded")
 
     # Load the model and define the loss function and optimizer
-    model = load_model(modelname=settings.model)
+    model = load_model(modelname=settings.model, freeze=settings.freeze)
     criterion = set_loss_function(loss=settings.loss)
     optimizer = set_optimizer(optimizer=settings.optimizer, lr=settings.lr)
 
@@ -113,9 +119,9 @@ if __name__ == '__main__':
 
     num_epochs = settings.num_epochs
     if settings.train:
-        best_model, val_accuracies = train(model, criterion, optimizer, num_epochs, train_dataloader, valid_dataloader)
+        best_model, val_accuracies = train(model, criterion, optimizer, num_epochs, train_dataloader, valid_dataloader, settings.modelname)
     else:
-        model.load_state_dict(torch.load('./models/best_densenet_model.pth'))
+        model.load_state_dict(torch.load(f'./models/{settings.modelname}.pth'))
         best_model = model
     test_acc = eval_model(best_model, test_dataloader)
     print(f'test accuracy: {test_acc}')
