8bfa448de5f852232148ec66f69e17b1b7a85887
Paul Hilders <lcur1028@test3.irc.sara.nl.irc.sara.nl>
2022-10-06 11:48:53 +0200

    Updated Dataloader to match branch Angela

--- Dataloader.py
+++ Dataloader.py
@@ -3,27 +3,54 @@ from torch.utils.data import Dataset
 import h5py
 import matplotlib.pyplot as plt
 
-from tqdm import tqdm
+class PCAMDataset(Dataset):
+    def __init__(self, h5pyfile_x, h5pyfile_y, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.pcam_x_frame = h5py.File(h5pyfile_x, 'r')['x']
+        print(self)
+        self.pcam_y_frame = h5py.File(h5pyfile_y, 'r')['y']
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.pcam_x_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        image = self.pcam_x_frame[idx]
+        label = self.pcam_y_frame[idx]
+
+        image = torch.from_numpy(image).long()
+        label = torch.from_numpy(label).long()
+
+        image = image.permute(2, 0, 1).byte() / 255
+        label = label.squeeze()
+
+        if self.transform:
+            image = self.transform(image)
+
+        sample = {'image': image, 'label': label}
+
+        return sample
 
 
-def getDataLoaders():
-    f = h5py.File('pcamv1/camelyonpatch_level_2_split_train_x.h5', 'r')
-    trainset_x = f['x']
-    f = h5py.File('pcamv1/camelyonpatch_level_2_split_train_y.h5', 'r')
-    trainset_y = f['y']
-    f = h5py.File('pcamv1/camelyonpatch_level_2_split_test_x.h5', 'r')
-    testset_x = f['x']
-    f = h5py.File('pcamv1/camelyonpatch_level_2_split_test_y.h5', 'r')
-    testset_y = f['y']
-    f = h5py.File('pcamv1/camelyonpatch_level_2_split_valid_x.h5', 'r')
-    validset_x = f['x']
-    f = h5py.File('pcamv1/camelyonpatch_level_2_split_valid_y.h5', 'r')
-    validset_y = f['y']
-    train_dataloader = torch.utils.data.DataLoader([[trainset_x[i], trainset_y[i]] for i in tqdm(range(len(trainset_y)), desc="Loading Training data")],
+def getDataLoaders(transform=None):
+    train_dataset = PCAMDataset('pcamv1/camelyonpatch_level_2_split_train_x.h5', 'pcamv1/camelyonpatch_level_2_split_train_y.h5', transform=transform)
+    valid_dataset = PCAMDataset('pcamv1/camelyonpatch_level_2_split_valid_x.h5', 'pcamv1/camelyonpatch_level_2_split_valid_y.h5', transform=transform)
+    test_dataset = PCAMDataset('pcamv1/camelyonpatch_level_2_split_test_x.h5', 'pcamv1/camelyonpatch_level_2_split_test_y.h5', transform=transform)
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,
                                                    shuffle=True, batch_size=64)
-    test_dataloader = torch.utils.data.DataLoader([[testset_x[i], testset_y[i]] for i in tqdm(range(len(testset_y)), desc="Loading Test data")],
-                                                   shuffle=True, batch_size=64)
-    valid_dataloader = torch.utils.data.DataLoader([[validset_x[i], validset_y[i]] for i in tqdm(range(len(validset_y)), desc="Loading Validation data")],
+    test_dataloader = torch.utils.data.DataLoader(test_dataset,
+                                                   shuffle=False, batch_size=64)
+    valid_dataloader = torch.utils.data.DataLoader(valid_dataset,
                                                    shuffle=True, batch_size=64)
     return train_dataloader, test_dataloader, valid_dataloader
 
@@ -55,11 +82,9 @@ def test_print_with_label(train_dataloader, label=1):
     plt.savefig(f'./imgs/exampledata_label{label}', bbox_inches='tight')
     plt.show()
 
+
 if __name__ == '__main__':
-    train_dataloader, test_dataloader, valid_dataloader = getDataLoaders()
+    train_dataloader, test_dataloader, valid_dataloader = getDataLoaders(transform=None)
     # test_print(train_dataloader)
     test_print_with_label(train_dataloader, label=1)
     test_print_with_label(train_dataloader, label=0)
-
-
-
