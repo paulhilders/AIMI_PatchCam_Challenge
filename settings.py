99d878830f5e3de50fe6d93a529ea0dc863181d2
Sarah de Boer <sarahdeboer@kpnmail.nl>
2022-10-09 20:36:37 +0200

    added TTA

--- settings.py
+++ settings.py
@@ -1,14 +1,21 @@
-import torchvision
+import torchvision.transforms as T
+import torch
 
 model = 'densenet201'
 loss = 'cross_entropy'
 optimizer = 'adam'
 lr = 0.0001
 num_epochs = 30
-train = True
+train = False
 freeze = False
-modelname = 'densenet201_cropped'
+modelname = 'densenet201_10epochs'
 eval_metric = 'accuracy'
-transform = torchvision.transforms.Compose([
-                                               torchvision.transforms.CenterCrop(32),
-                                           ])
+TTA = True
+crop_transform = None  # T.Compose([T.CenterCrop(32),])
+DA_transform = None
+TTA_transform = T.RandomApply(transforms=torch.nn.ModuleList([
+                    # T.RandomGrayscale(p=0.5),
+                    T.RandomHorizontalFlip(p=0.5),
+                    T.RandomVerticalFlip(p=0.5),
+                    T.RandomRotation(degrees=90),
+                ]), p=0.8)
